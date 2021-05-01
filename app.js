31ec37dd4d966123e3969a5c9474976c0f71049e
Suman <pulicheri.suman@broadcom.com>
2021-05-01 22:46:21 +0530

    fixed issues

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const webpush = require('web-push');
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 app.use(express.json());
 app.use(express.static(__dirname + '/client'));
 
