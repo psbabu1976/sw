4f65787440aaef7c7e303ed80ac79b5c6f2f1feb
psbabu1976 <psbabu1976@gmail.com>
2021-05-01 19:33:53 +0530

    Update sw.js

--- client/sw.js
+++ client/sw.js
@@ -15,6 +15,6 @@ self.addEventListener('push', function(e) {
       ]
     };
     e.waitUntil(
-      self.registration.showNotification('Hello world!', options)
+      self.registration.showNotification('Haha!', options)
     );
   });
